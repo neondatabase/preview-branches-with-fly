6287a4ccb2b370ed6e4b4e9b811c1fbd5448f55d
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-02-01 19:43:57 +0200

    Remove unnecessary line in getUsers function

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,7 +4,6 @@ import { users } from "../db/schema";
 export const getUsers = async () => {
   try {
     const allUsers = await db.select().from(users);
-
     return { users: allUsers };
   } catch (error) {
     throw new Error(error);
