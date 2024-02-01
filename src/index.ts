1b04334e2d6cbba57115aa18e2efd4a229d1a70c
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-02-01 08:54:56 +0200

    Add initial server setup

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import Fastify from "fastify";
+import { db } from "./db";
+import { users } from "./db/schema";
+
+const PORT = Number(process.env.PORT) || 3000;
+
+const fastify = Fastify();
+
+fastify.get("/", async function handler(_, reply) {
+  try {
+    const allUsers = await db.select().from(users);
+    return { users: allUsers };
+  } catch (error) {
+    console.log(error);
+    return reply.status(500).send({ error: `"Something went wrong":${error}` });
+  }
+});
+
+const startServer = async () => {
+  try {
+    await fastify.listen({ port: PORT, host: "0.0.0.0" });
+    console.log(`Server listening on port ${PORT} 🚀`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
